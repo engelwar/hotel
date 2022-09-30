57d8f8e6e2e6703895bc14028296b7bfef8f556c
sullca22 <juliocesarmancillaruiz@gmail.com>
2022-09-30 18:24:11 -0400

    UP

--- Consulta/registrar_reserva.php
+++ Consulta/registrar_reserva.php
@@ -29,23 +29,23 @@ $queryEspecialistas = mysqli_query($con, $sqlEspecialistas);
     <a class="navbar-brand ps-3" href="../logout.php">Cerrar Sesion</a>
   </div>
 </nav>
-<a href="index.php" class="position-absolute ml-4"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="backward" class="svg-inline--fa fa-backward fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M11.5 280.6l192 160c20.6 17.2 52.5 2.8 52.5-24.6V96c0-27.4-31.9-41.8-52.5-24.6l-192 160c-15.3 12.8-15.3 36.4 0 49.2zm256 0l192 160c20.6 17.2 52.5 2.8 52.5-24.6V96c0-27.4-31.9-41.8-52.5-24.6l-192 160c-15.3 12.8-15.3 36.4 0 49.2z"></path></svg>Volver a Consultas</a>
+<a href="index.php" class="position-absolute ml-4"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="backward" class="svg-inline--fa fa-backward fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+    <path fill="currentColor" d="M11.5 280.6l192 160c20.6 17.2 52.5 2.8 52.5-24.6V96c0-27.4-31.9-41.8-52.5-24.6l-192 160c-15.3 12.8-15.3 36.4 0 49.2zm256 0l192 160c20.6 17.2 52.5 2.8 52.5-24.6V96c0-27.4-31.9-41.8-52.5-24.6l-192 160c-15.3 12.8-15.3 36.4 0 49.2z"></path>
+  </svg>Volver a Consultas</a>
 <div class="jumbotron">
   <div class="container text-center w-50 shadow-lg p-4">
     <div class="text-center mb-4">
-      <h2>Registrar Consulta</h2>
+      <h2>Registrar Reserva</h2>
     </div>
     <form action="registrar_hora.php" method="$_POST">
       <div class="border p-4">
         <div class="col-md-12 mt-2">
           <label for="">
-            <h4>Especialista en:</h4>
+            <h4>Fecha:</h4>
           </label>
-          <select class="form-select w-100" aria-label="Default select example" name="especialidad">
-            <?php while ($dataEspecialistas = mysqli_fetch_array($queryEspecialistas)) { ?>
-              <option value="<?php echo $dataEspecialistas['ESPECIALIDAD']; ?>"><?php echo $dataEspecialistas['ESPECIALIDAD']; ?></option>
-            <?php } ?>
-          </select>
+          <div class="w-50 m-auto">
+            <input id="fecha" type="date" class="form-control form-control-sm" name="fecha">
+          </div>
         </div>
         <div class="col-md-12 mt-2">
           <button type="submit" class="btn btn-danger">Consultar Medicos</button>
